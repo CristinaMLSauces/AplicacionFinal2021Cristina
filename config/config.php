95118952bee7a8e96354bf564e7b52b619b4d6af
Cristina Manjon Lacalle <cristina.manlac@educa.jcyl.es>
2021-04-21 11:54:38 +0200

    Creo el proyecto LoginLogoffPOOMulticapa, con sus directorios, con el config, la libreria y los script para la base de datos / 21-04-2021

---
+++ config/config.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once "../core/210322ValidacionFormularios.php";
+
+require_once "modelo/Usuario.php";
+require_once "modelo/UsuarioPDO.php";
+require_once "modelo/DBPDO.php";
+
+$controladores = [
+    "login" => "controlador/cLogin.php",
+    "inicio" => "controlador/cInicio.php",
+    "registro" => "controlador/cRegistro.php"
+];
+
+$vistas = [
+    "layout" => "vista/layout.php",
+    "login" => "vista/vLogin.php",
+    "inicio" => "vista/vInicio.php",
+    "registro" => "vista/vRegistro.php"
+];
+?>
---
+++ scriptDB/BorraDAW207DBProyectoTema5_Desarollo.sql
@@ -0,0 +1,5 @@
+-- BORRADO BASE DE DATOS DAW207DBDepartamentos
+DROP DATABASE DAW207DBLoginLogoffPOOMulticapa;
+
+-- BORRADO USUARIO usuarioDAW207DBDepartamentos
+DROP USER usuarioDAW207DBLoginLogoff;
